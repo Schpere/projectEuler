1e65c5af3cb823fa7c52163dfb5d5da077d8795e
schpere <harald.sperre@gmail.com>
2015-09-24 14:55:01 +0200

    Or not

--- helpers.py
+++ helpers.py
@@ -2,23 +2,33 @@ from random import randrange
 from random import sample
 
 def jacobiSym(a,n):
-    if a % n == 0:
-        return 0
-    elif a > 10 or n > 10:
-        if a > n:
-            return jacobiSym(a % n, n)
-        if ([a % 4, n % 4] == [3,3]):
-            return -jacobiSym(n,a)
-        return jacobiSym(n,a)
-    primes = [2,3,5,7]
-    if n in primes:
-        quads = [i**2 % n for i in range(1,int(n/2)+1)]
-        if a % n in quads:
-            return 1
-        return -1
-    for p in primes:
-       if n % p == 0:
-           return jacobiSym(a,p) * jacobiSym(a,n/p)
+    fortegn = 1
+    while a > 10 or n > 10:
+        if a == 1 or n == 1:
+            return fortegn
+        elif a == 2:
+            if (n % 8) in [1,7]:
+                return fortegn
+            return -fortegn
+        elif a % n == 0:
+            return 0
+        elif a > n:
+            a = a % n
+        else:
+            if ([a % 4, n % 4] == [3,3]):
+                fortegn *= -1
+            a,n = n,a
+    results = [[1,1,1,1,1,1,1,1,1,1],[1,0,-1,0,-1,0,1,0,1,0],[1,-1,0,1,-1,0,-1,-1,0,1],[1,0,1,0,1,0,1,0,1,0],[1,-1,-1,1,0,1,-1,-1,1,0],[1,0,0,0,1,0,-1,0,0,0],[1,1,1,1,-1,1,0,1,1,-1],[1,0,-1,0,-1,0,1,0,1,0],[1,1,0,1,1,0,1,1,0,1],[1,0,1,0,0,0,-1,0,1,0]]
+    return fortegn*results[n+1][a+1]
+#    primes = [2,3,5,7]
+#    if n in primes:
+#        quads = [i**2 % n for i in range(1,int(n/2)+1)]
+#        if a % n in quads:
+#            return fortegn
+#        return -fortegn
+#    for p in primes:
+#       if n % p == 0:
+#           return fortegn * jacobiSym(a,p) * jacobiSym(a,n/p)
 
 def jacobiSymbol(a,n):
     if (a == 1 or n == 1):
@@ -51,7 +61,7 @@ def primeQ(n):
             if jacobiSym(a,n) % n != a**((n-1)/2) % n:
                 return False
         return True
-    for i in range(100):
+    for i in range(10):
         a = randrange(n)
         if jacobiSym(a,n) % n != a**((n-1)/2) % n:
             return False
