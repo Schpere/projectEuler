f62fc0ff399b5fff6abf03fa7535d7a5b3bf9c19
schpere <harald.sperre@gmail.com>
2015-09-27 12:41:50 +0200

    ting er rare

--- problem012.py
+++ problem012.py
@@ -1,10 +1,29 @@
-from helpers import numFactors
+from helpers import sieve,primeQ
+primes = sieve(1000000)
 n = 1
 divisors = 0
 n_2 = 0
 
-while divisors <= 1000:
-    n_1,n_2 = n_2,numFactors(n+1)
+while divisors < 1000:
+    prime_divisors = list()
+    n_1 = n_2
+    while n >= 2*primes[-1]:
+        p = primes[-1] + 2
+        while not primeQ(p):
+            p += 2
+        primes.append(p)
+    n_2 = n
+    for p in primes:
+        if n_2 % p == 0:
+            prime_divisors.append(p)
+            prime_divisors.append(2)
+            n_2 /= p
+        while n_2 % p == 0:
+            n_2 /= p
+            prime_divisors[-1] += 1
+        if n_2 == 1:
+            break
+    n_2 = reduce(lambda x, y : x * y, prime_divisors[1::2],1)
     divisors = n_1*n_2
     n += 1
 print n*(n-1)/2
