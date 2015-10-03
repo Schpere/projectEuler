691c3868fe4c45e61261355c64b3a03123a8ac96
schpere <harald.sperre@gmail.com>
2015-10-03 15:30:28 +0200

    Should I use trees to do problem 14 the other way around?

--- problem014.py
+++ problem014.py
@@ -1,21 +1,24 @@
-chainlengths = {}
+#chainlengths = {}
 maxest = 0
 best = 0
+#for i in xrange(20):
+#    chainlengths[2**i] = i
 
-for i in range(1000000):
+for i in xrange(1000):
     j = i
-    this_terms = 1
+    this_terms = 0
     while j != 1:
-        if j in chainlengths:
-            this_terms = chainlengths[j] + this_terms - 1
-            j = 1
-        elif j % 2 == 0:
+#        if j in chainlengths:
+#            this_terms = chainlengths[j] + this_terms - 1
+#            j = 1
+        if j % 2 == 0:
             j /= 2
         else:
-            j = 3*j + 1
+            j = 3 * j + 1
         this_terms += 1
     if this_terms > maxest:
         best = i
         maxest = this_terms
-        chainlengths[best] = maxest
+#    if i < 500000:
+#        chainlengths[i] = this_terms
 print best
