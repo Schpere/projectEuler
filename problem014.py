f9a488c7078847aa32ff0f9de953b48e3cd33355
schpere <harald.sperre@gmail.com>
2015-10-04 19:13:33 +0200

    Problem 14 does not like me

--- problem014.py
+++ problem014.py
@@ -1,24 +1,30 @@
-#chainlengths = {}
-maxest = 0
-best = 0
-#for i in xrange(20):
-#    chainlengths[2**i] = i
+from collections import deque
+que = deque()
+chain = {1:1}
+counter = 0
+counterI = 0
 
-for i in xrange(1000):
-    j = i
-    this_terms = 0
-    while j != 1:
-#        if j in chainlengths:
-#            this_terms = chainlengths[j] + this_terms - 1
-#            j = 1
-        if j % 2 == 0:
-            j /= 2
-        else:
-            j = 3 * j + 1
-        this_terms += 1
-    if this_terms > maxest:
-        best = i
-        maxest = this_terms
-#    if i < 500000:
-#        chainlengths[i] = this_terms
-print best
+que.append(1)
+while counter < 10 and que:
+    i = min(que)
+    que.remove(i)
+    print i
+    chain[2*i] = 1 + chain[i]
+    if i < 12E9: que.append(2*i)
+    if 2*i < 10:
+        counter += 1
+        if counter % 1000 == 0: print counter
+    if (i-1) % 6 == 3 and (i-1)/3 not in chain:
+        chain[(i-1)/3] = 1 + chain[i]
+        if i < 12E9: que.append((i-1)/3)
+        if (i-1)/3 < 10:
+            counter += 1
+            if counter % 1000 == 0: print counter
+maximus = 0
+print chain
+for i in range(1,int(1E6)):
+    print i
+    if chain[i] > maximus:
+        maximus = chain[i]
+
+print maximus
