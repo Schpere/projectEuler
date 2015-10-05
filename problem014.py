24772cc237701a175c10156efe893f867f156a3a
schpere <harald.sperre@gmail.com>
2015-10-05 12:06:59 +0200

    It finally works! But why?? o.O

--- problem014.py
+++ problem014.py
@@ -1,30 +1,57 @@
 from collections import deque
 que = deque()
-chain = {1:1}
 counter = 0
-counterI = 0
 
-que.append(1)
-while counter < 10 and que:
-    i = min(que)
-    que.remove(i)
-    print i
-    chain[2*i] = 1 + chain[i]
-    if i < 12E9: que.append(2*i)
-    if 2*i < 10:
-        counter += 1
-        if counter % 1000 == 0: print counter
-    if (i-1) % 6 == 3 and (i-1)/3 not in chain:
-        chain[(i-1)/3] = 1 + chain[i]
-        if i < 12E9: que.append((i-1)/3)
-        if (i-1)/3 < 10:
-            counter += 1
-            if counter % 1000 == 0: print counter
-maximus = 0
-print chain
-for i in range(1,int(1E6)):
-    print i
-    if chain[i] > maximus:
-        maximus = chain[i]
+class Collatz:
+    def __init__(self,value,parent):
+        self.value = value
+        if parent != None:
+            self.level = 1 + parent.level
+        else:
+            self.level = 1
+        self.parent = parent
 
-print maximus
+lengths = [0 for _ in xrange(int(1E6))]
+root = Collatz(1,None)
+que.append(root)
+longest = root
+
+while counter < 1E6 and que:
+    node = que.popleft()
+    if node.value < 1E6:
+        lengths[node.value - 1] = node.level
+        if node.level > longest.level:
+            longest = node
+    if (node.value - 1) % 6 == 3 and node.value > 4:
+        node.rightchild = Collatz((node.value - 1)/3,node)
+        if node.rightchild.value < 1E6: counter += 1
+        que.append(node.rightchild)
+    node.leftchild = Collatz(node.value * 2,node)
+    if node.leftchild.value < 1E6: counter += 1
+    if node.leftchild.value < 1E3: que.append(node.leftchild)
+
+
+
+print [i for i, e in enumerate(lengths) if e != 0]
+
+
+maximus = longest.level
+longValue = longest.value
+
+for i in xrange(int(1E6)):
+    length = lengths[i]
+    if length == 0:
+        j = i+1
+        while True:
+            length += 1
+            if j % 2 == 0:
+                j /= 2
+            else:
+                j = 3*j + 1
+            if j < 1E6 and lengths[j-1] > 0:
+                length += lengths[j-1]
+                break
+        lengths[i] = length
+        if lengths > maximus:
+            longValue = i+1
+print max(xrange(len(lengths)),key=lengths.__getitem__) + 1
