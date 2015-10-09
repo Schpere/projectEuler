c61ca0a32af4dbdc6e60c808a524676e151cd756
schpere <harald.sperre@gmail.com>
2015-10-09 13:23:10 +0200

    Add some junk to problem 24. Try recursion

--- problem024.py
+++ problem024.py
@@ -1,4 +1,10 @@
 from math import factorial
 n = factorial(9)
-
 current = [0,9,8,7,6,5,4,3,2,1]
+def next(array):
+    for i in range(len(array)-1,-1,-1):
+        if array[i] = min(array):
+
+while n < 1E6:
+
+    n += 1
