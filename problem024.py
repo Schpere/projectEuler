6693663517e3b39d92704f0c68357064bdb71fbe
schpere <harald.sperre@gmail.com>
2015-10-19 17:02:38 +0200

    Tried to make next(array) do something

--- problem024.py
+++ problem024.py
@@ -2,8 +2,20 @@ from math import factorial
 n = factorial(9)
 current = [0,9,8,7,6,5,4,3,2,1]
 def next(array):
-    for i in range(len(array)-1,-1,-1):
-        if array[i] = min(array):
+    liste = array
+    last = liste[-1]
+    for i in range(len(liste)-1,-1,-1):
+        if liste[i] < last:
+            subarray = liste[i:]
+            break
+    print subarray
+    temp = subarray[-1]
+    for j in range(len(subarray)-1,-1,-1):
+        subarray[j] = subarray[j-1]
+    subarray[0] = temp
+    liste[i:] = subarray
+    print subarray
+    return liste 
 
 while n < 1E6:
 
