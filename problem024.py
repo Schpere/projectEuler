109525aef872fdadafcfb53171a7abb06404ef18
schpere <harald.sperre@gmail.com>
2015-11-27 12:27:54 +0100

    add problem 24

--- problem024.py
+++ problem024.py
@@ -1,22 +1,16 @@
 from math import factorial
 n = factorial(9)
 current = [0,9,8,7,6,5,4,3,2,1]
-def next(array):
-    liste = array
-    last = liste[-1]
-    for i in range(len(liste)-1,-1,-1):
-        if liste[i] < last:
-            subarray = liste[i:]
-            break
-    print subarray
-    temp = subarray[-1]
-    for j in range(len(subarray)-1,-1,-1):
-        subarray[j] = subarray[j-1]
-    subarray[0] = temp
-    liste[i:] = subarray
-    print subarray
-    return liste 
+def neste(array):
+    for i in range(1,len(array)):
+        if array[-i] > array[-i-1]:
+            return array[:-i-1] + [array[-i]] + sorted([array[-i-1]] + array[i+1:])
+
 
 while n < 1E6:
 
     n += 1
+    current = neste(current)
+    print current
+
+print current
